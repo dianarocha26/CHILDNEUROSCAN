18dd5a28a95d6cf38ec4b7f49a87e1e915384356
dianarocha26 <dianarocha26@hotmail.com>
2026-02-24 13:27:12 -0500

    Added .env.example

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import './styles/index.css';
+import App from './App.tsx';
+import './index.css';
+
+import { AuthProvider } from './contexts/AuthContext.tsx';
+import { ThemeProvider } from './contexts/ThemeContext.tsx';
+import { LanguageProvider } from './contexts/LanguageContext.tsx';
+import { ErrorBoundary } from './components/ErrorBoundary.tsx';
+import { registerServiceWorker } from './lib/serviceWorker';
+
+registerServiceWorker();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <ThemeProvider>
+        <AuthProvider>
+          <LanguageProvider>
+            <App />
+          </LanguageProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
   </StrictMode>
 );
